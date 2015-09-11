1e72a0a30c01bb9be6a0381aa298dc9d1cf44097
Atsushi Nagase <a@ngs.io>
2015-09-11 17:37:13 +0900

    Scaffold Books

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150911082140) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "author",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
